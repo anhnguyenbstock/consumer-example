bbb1c81df32036f3479c146d1094bb7dcf536e17
Anh Nguyen <anh@bstock.com>
2021-06-10 14:08:06 +0700

    commit 4

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const fs = require('fs');
 
 // Delete existing Contract
+const path = `./pacts/${process.env.CONSUMER_NAME}-${process.env.PROVIDER_NAME}.json`;
+
 module.exports = async function() {
-    fs.unlink(`./pacts/${process.env.CONSUMER_NAME}-${process.env.PROVIDER_NAME}.json`, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("The existing Contract is deleted.");
-    });
+    if (fs.existsSync(path)) {
+        fs.unlink(path, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("The existing Contract is deleted.");
+        });
+    }
 }
